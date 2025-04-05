b38f6cc56754237ad83e3825fb844cb675776bb3
dhanashree3029 <dhanashreevd1999@gmail.com>
2025-04-05 21:38:52 +0530

    Add files via upload

---
+++ set.dict.py
@@ -0,0 +1,248 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 26 19:29:39 2025
+
+@author: HP
+"""
+
+#union of two sets.
+#unique elements from the given sets.
+#set1,set2={1,2,3},{3,4,5,2}
+
+#set1=set1.union(set1,set2)
+
+#print(set1)
+
+#intersection
+#matched elements from the given sets.
+#set1=set1.intersection(set1,set2)
+
+#difference
+#set1=set1.difference(set2)
+#set2=set2.difference(set1)
+#print(set2)
+
+#symmetric_difference
+#it will skip the matched elemnts from the given sets.
+#set1=set1.symmetric_difference(set2)
+#print(set1)
+
+#issubset, issuperset
+
+#s1={1,2,3,4,5,6}
+#s2={1,2,3,4}
+#issubset will return true when all the elements from s1
+#are present in the set s2e;se return False print(s1.issubset(s2))
+
+#issubset will return True when all the elements from s2
+#are present in the s1,else return False print(s1.issuperset(s2))
+
+
+#DICTIONARY
+
+#d1={}
+#print(d1)
+
+#d2={91:'IND',1:'USA'}
+
+#dept={101:'HR',102:'Sales',103:'Admin'}
+
+#d3={1:'Amol','marks':[78,67,66]}
+
+#inner round bracket is to define one parameter.
+#d4=dict(([1,500],[2,499],[3,350]))
+#print(d4)
+
+#fruit={}
+
+#fruit['apple']=200
+#fruit['orange']=150
+
+#print(fruit)
+
+#fruit['orange']=250
+#print(fruit)
+#print(fruit.get('apple'))
+
+#print(dept.get(102))
+
+#dept={101:'HR',102:'Sales',103:'Admin'}
+
+#iterate using for in loop
+# .keys()
+
+#for key in dept.keys():
+    #print(key)
+    
+# .values() ->values will be extracted.
+
+#for value in dept.values():
+    #print(value)
+    
+#print both keys and values ->items()
+
+#for k,v in dept.items():
+    #print(f'{k}: {v}')
+    
+#dept.clear()
+#print dept
+
+#del dept
+#del dept[103]  #103 is key
+#print(dept)
+
+#**kwargs
+#def display(**data):
+    #print(data)
+    #for k,v in data.items():
+        #print(f'{k}:{v}')
+        
+#display(age=30,city='Pune',rollno=1,per=78.88)
+
+
+
+#class work.
+
+#1.create a dictionary to hold employee data like id, name, salary.
+
+#employee_data ={"Id": "101","Name":"siya","salary":"40000"}
+#print(employee_data)
+
+#print(employee_data.keys())
+#print(employee_data.values())
+#print(employee_data.items())
+
+
+#Create a dictionary to hold product data ;ike code, name, price.
+
+#dept ={"code": "200", "name": "rishi", "Price":"700"}
+#print(dept)
+
+#print(dept.keys())
+#print(dept.values())
+#print(dept.items())
+
+
+#Delete one product in the existing dictionary.
+#dict = {'name' :'rahul', 'class' :'5', 'age': '8'}
+#del dict['age']
+#print(dict)
+
+#add one product in the dict.
+#employees1 ={1:'amol',2:'rani'}
+#employees2 ={2:'jojo'}
+#print (employees1,employees2)
+
+
+
+# Assessments on Dictionary.
+
+# 1. Wap to combine two dictionary adding values for common keys.
+#d1={'a':100, 'b':200, 'c':300}
+#d2={'a':300, 'b':200, 'd':400}
+
+#d3=d1.copy()
+#d3.update(d2)
+
+#for key in d2:
+    #if key in d1:
+        #d3[key]=d2[key]+d1[key]
+#print(d3)
+
+
+# 2.wap in python to print all unique values in dictionary.
+
+#Original List:[{"V":"S001"},{"V":"S002"},{"VI":"S001"},{"VI":"S005"},
+               #{"VI":"S005"},{"V":"S009"}]
+#Unique values:{'S009','S002','S007','S005','S001'}
+
+#L = [{"V":"S001"},{"V":"S002"},{"VI":"S001"},{"VI":"S005"},{"VII":"S005"},
+     #{"V":"S009"}]
+     
+#print('Original List:',L)
+#unique_value = set(value for dic in L for val in dic.value())
+#print("Unique Values:",unique_value)
+
+
+#wap to create a dictionary from a string.
+#track the count of the letters from the string'
+#sample string :'w3resource'
+#Expected output:{'3': 1, 's': 1,'r': 2,'u': 1,'w': 1,'c': 1,'e': 2,'o':1}
+
+#string ="Hello world"
+
+#dict = {}
+
+#for i in string:
+    #if i in dict:
+        #continue
+    #elif i==" ":
+      #continue
+    #else:
+      #dict[i]=string.count(i)
+#print(dict)
+
+
+
+#Merge following two python dictionaries into one.
+#Get the key corresponding to the minimum value from the following dictionary.
+# sampleDict = {'Physics':82,'Math':65,'history':75}
+#expected output:Math.
+
+#dict1 ={'physics':82,'math':65,'history':75}
+#list1=[]
+#list1 = min(dict1,key = dict1.get)
+#print(list1)
+
+
+#combine two dictionary adding values for common keys
+#Input: dict1 ={'a':12, 'for'':25,'c':9}
+# dict2='python':100,'java':200,'for':300}
+#output:{'for':325,'python':100,'java':200}
+
+#d1={'a':12,'for':25,'c':9}
+#d2={'python':100,'java':200,'for':300}
+
+#d3=d1.copy()
+#d3.update(d2)
+
+#for key in d2:
+    #if key in d1:
+        #d3[key]=d2[key]+d1[key]
+#print(d3)
+
+
+#dict1={101:{"Apple":10,"Mango":5},
+#102:{"Apple":15,"Mango":8,"Cherry":5},
+#103:{"Apple":10}}
+#output Dict2={"Apple"35,"Mango":13,"Cherry":5},
+
+d1={"Apple":10,"Mango":5}
+d2={"Apple":15,"Mango":8,"Cherry":5}
+d3={"Apple":10}
+
+d4=d1.copy()+d2.copy()
+d4.update(d2+d3)
+
+for key in d2, d3:
+    if key in d1:
+        d4[key]=d3[key]+d2[key]+d1[key]
+print(d4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
